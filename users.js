692d5537cd41d45d88cbf8d9b64abc14d3f705e4
Convextion <33188170+Convextion@users.noreply.github.com>
2017-11-09 23:12:16 -0500

    Bug error
    I've got autismo

--- users.js
+++ users.js
@@ -165,13 +165,10 @@ function importUsergroups() {
 	for (let i in usergroups) delete usergroups[i];
 
 	FS('config/usergroups.csv').readTextIfExists().then(data => {
-		
-		for (const row of data split.split("\n")) {
-		
+		for (const row of data.split("\n")) {
 			if (!row) continue;
 			let cells = row.split(",");
-			usergroups[toId(cells[0])] = (cells[1] ||
-Config.groupsranking[0]) + cells[0];						      
+			usergroups[toId(cells[0])] = (cells[1] || Config.groupsranking[0]) + cells[0];						      
 		}
 	});
 }
